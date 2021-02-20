9ff774cdbe9a29a16a67f5685e4d96c5e9b60963
doester1 <60227710+doester1@users.noreply.github.com>
2021-02-20 09:53:22 +0100

    zmiana tokenu

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config({ path: __dirname + "/./../../.env" })
 
 module.exports = {
-  TOKEN: ODEyNjA3MTQ5ODkzMDkxMzI5.YDDNYg.ksSKiOmsPoFdxbYCFDhngXwasTs,
+  TOKEN: ODEyNjA3MTQ5ODkzMDkxMzI5.YDDNYg.V_H6ofpFjvIMHgrKeHaktSNwa04,
   PREFIX: =,
   OWNER: process.env.owner,
   CORONA_API: "https://corona.lmao.ninja/v2",
