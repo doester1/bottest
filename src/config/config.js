db10091ed0ece3cc74974d462404de2f5bd00914
doester1 <60227710+doester1@users.noreply.github.com>
2021-02-20 09:51:54 +0100

    zmiana Prefix i tokena

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv").config({ path: __dirname + "/./../../.env" })
 
 module.exports = {
-  TOKEN: process.env.token,
-  PREFIX: process.env.prefix,
+  TOKEN: ODEyNjA3MTQ5ODkzMDkxMzI5.YDDNYg.ksSKiOmsPoFdxbYCFDhngXwasTs,
+  PREFIX: =,
   OWNER: process.env.owner,
   CORONA_API: "https://corona.lmao.ninja/v2",
 }
